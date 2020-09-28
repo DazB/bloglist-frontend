6293b89da3b3f99c5de6d1414b55154b0b09d013
Dariush Bahri <dazb@alumni.york.ac.uk>
2020-09-28 22:51:45 +0800

    5.2: bloglist frontend, step2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+const storageKey = 'loggedBlogappUser'
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
@@ -13,6 +15,13 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem(storageKey)
+    if (loggedUserJSON) {
+      setUser(JSON.parse(loggedUserJSON))
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
@@ -21,6 +30,7 @@ const App = () => {
         password,
       })
 
+      window.localStorage.setItem(storageKey, JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -57,7 +67,17 @@ const App = () => {
   const blogList = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button
+          onClick={() => {
+            setUser(null)
+            window.localStorage.removeItem(storageKey)
+          }}
+        >
+          logout
+        </button>
+      </p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
