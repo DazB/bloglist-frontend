9d740bfa5bac01bb63195a04d6701625abbe70e8
Dariush Bahri <dazb@alumni.york.ac.uk>
2020-10-09 22:30:41 +0800

    5.9*: Blog list frontend, step9

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ const App = () => {
 
   useEffect(() => {
     const fetchBlogs = async () => {
-      const blogs = await blogService.getAll()
-      setBlogs(blogs)
+      const fetchedBlogs = await blogService.getAll()
+      setBlogs(fetchedBlogs)
     }
     fetchBlogs()
   }, [])
@@ -132,9 +132,13 @@ const App = () => {
         <h2>create new</h2>
         <BlogFrom createBlog={createBlog} />
       </Togglable>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} addLike={addLike} />
-      ))}
+      {blogs
+        .sort((blog1, blog2) => {
+          return blog2.likes - blog1.likes
+        })
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} addLike={addLike} />
+        ))}
     </div>
   )
 
